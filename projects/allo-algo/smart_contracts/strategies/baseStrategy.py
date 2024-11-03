72232a6df4436d8eefca3ae5350c79e29c872d04
Nithin-Varma <mnithin1422@gmail.com>
2024-11-03 23:23:58 +0700

    base strategy

--- projects/allo-algo/smart_contracts/strategies/baseStrategy.py
+++ projects/allo-algo/smart_contracts/strategies/baseStrategy.py
@@ -1,111 +1,125 @@
 import algopy
-from algopy import Contract, ARC4Contract, String, Account, UInt64, UInt256
+from algopy import Contract, ARC4Contract, String, Account, UInt64, arc4, Bytes
 from algopy.arc4 import DynamicArray, Address, Struct
 
 
-class PayoutSummary(Struct):
-    recipientAddress: Address
-    amount: UInt256
+class PayoutSummary(arc4.Struct):
+    recipientAddress: arc4.Address
+    amount: arc4.UInt64
 
 class BaseStrategy(ARC4Contract):
-    def __init__(self, alloContractId: Account, strategyId:String) -> None:
-        self.alloContractId = alloContractId
+    def __init__(self, _alloContractId: Address, strategyId:String) -> None:
+        self.alloContractId = _alloContractId
         self.strategyId = strategyId
-        self.poolActive: bool = false
+        self.poolActive: bool = False
         self.poolId = UInt64(0)
-        self.poolAmount = UInt256(0)
+        self.poolAmount = UInt64(0)
 
+    @algopy.subroutine
     def __OnlyAllo(self) -> None :
-        assert(algopy.Txn.sender == self.alloContractId, "only allo contract can call this function")
+        assert algopy.Txn.sender == self.alloContractId, "only allo contract can call this function"
 
+    @algopy.subroutine
     def __OnlyInitialized(self) -> None :
-        assert(self.poolId !=0, "pool is not initialized")
+        assert self.poolId !=0, "pool is not initialized"
 
-    def getAlloContractId(self) -> Account :
+    @arc4.abimethod()
+    def getAlloContractId(self) -> Address :
         return self.alloContractId
 
+    @arc4.abimethod()
     def getPoolId(self) -> UInt64 :
         return self.poolId
 
-    def getStrategyId(self) -> Account :
+    @arc4.abimethod()
+    def getStrategyId(self) -> String :
         return self.strategyId
 
-    def getPoolAmount(self) -> Account :
+    @arc4.abimethod()
+    def getPoolAmount(self) -> UInt64 :
         return self.poolAmount
 
+    @arc4.abimethod()
     def isPoolActive(self) -> bool :
         return self.poolActive
-    
-    def baseStrategy_init(self, _poolId: UInt256) -> None :
+
+    @arc4.abimethod()
+    def baseStrategy_init(self, _poolId: UInt64) -> None :
         self.__OnlyAllo()
-        assert(self.poolId == 0, "pool already initialized")
-        assert(_poolId != 0, "invalid pool id")
+        assert self.poolId == 0, "pool already initialized"
+        assert _poolId != 0, "invalid pool id" 
         self.poolId = _poolId
-    
-    def increasePoolAmount(self, _amount: UInt256) -> None :
-        self.__OnlyAllo()
-        self.poolAmount += _amoount
 
-    def registerRecipient(self, _data: Bytes, _sender: Account) -> Account :
+    @arc4.abimethod()
+    def increasePoolAmount(self, _amount: UInt64) -> None :
+        self.__OnlyAllo()
+        self.poolAmount += _amount
+
+        # receipient register
+    @arc4.abimethod()   
+    def registerRecipient(self, _data: Bytes, _sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def beforeRegisterRecipient(self, data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def afterRegisterRecipient(self, data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def register_Recipient(self, _data: Bytes, _sender: Account) -> None :
         self.__OnlyAllo()
         self.__OnlyInitialized()
-        _beforeRegisterRecipient(_data, _sender)
-        recipientId: Account = _registerRecipient(_data, _sender)
-        _afterRegisterRecipient(_data, _sender)
+        self.beforeRegisterRecipient(_data, _sender)
+        self.registerRecipient(_data, _sender)
+        self.afterRegisterRecipient(_data, _sender)
 
-    def allocate(self, _data: Bytes, _sender: Account) :
+    # allocate
+    @arc4.abimethod()
+    def allocate(self, _data: Bytes, _sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def beforeAllocate(self, data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def afterAllocate(self, data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def Allocate(self, _data: Bytes, _sender: Account) -> None:
         self.__OnlyAllo()
-        _beforeAllocate(_data, _sender)
-        _allocate(_data, _sender)
-        _afterAllocate(_data, _sender)
+        self.beforeAllocate(_data, _sender)
+        self.allocate(_data, _sender)
+        self.afterAllocate(_data, _sender)
 
-    def distribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, _sender: Account):
+    # distribute
+    @arc4.abimethod()   
+    def distribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, _sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def beforeDistribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def afterDistribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, sender: Account) -> None:
+        pass
+
+    @arc4.abimethod()
+    def Distribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, _sender: Account) -> None:
         self.__OnlyAllo()
         self.__OnlyInitialized()
-        _beforeDistribute(_recipientIds, _data, _sender)
-        _distribute(_recipientIds, _data, _sender)
-        _afterDistribute(_recipientIds,_data, _sender)
+        self.beforeDistribute(_recipientIds, _data, _sender)
+        self.distribute(_recipientIds, _data, _sender)
+        self.afterDistribute(_recipientIds,_data, _sender)
 
     # def getPayOuts(self, _recipientIds: DynamicArray[Address], _data: DynamicArray[Bytes]) :
     #     assert(_recipientIds.length == _data.length, "recipient and data length mismatch")
         
-        
-
-    # receipient register
-    @algopy.subroutine   
-    def _registerRecipient(self, _data: Bytes, _sender: Account) -> Account:
-
-    @algopy.subroutine
-    def _beforeRegisterRecipient(self, data: Bytes, sender: Account):
-        pass
-
-    @algopy.subroutine
-    def _afterRegisterRecipient(self, data: Bytes, sender: Account):
-        pass
-
-    # allocate
-    @algopy.subroutine   
-    def _allocate(self, _data: Bytes, _sender: Account) -> Account:
-
-    @algopy.subroutine
-    def _beforeAllocate(self, data: Bytes, sender: Account):
-        pass
-
-    @algopy.subroutine
-    def _afterAllocate(self, data: Bytes, sender: Account):
-        pass
-
-    # distribute
-    @algopy.subroutine   
-    def _distribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, _sender: Account):
-
-    @algopy.subroutine
-    def _beforeDistribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, sender: Account):
-        pass
-
-    @algopy.subroutine
-    def _afterDistribute(self, _recipientIds: DynamicArray[Address], _data: Bytes, sender: Account):
-        pass
 
     # @algopy.subroutine
     # def _getPayOut(self, _recipientId: Address, _data: Bytes):
